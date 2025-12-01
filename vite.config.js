a8ba775adfa23323e09c5105eac6324e3460fdab
Jefferson Khal El Kurniawan <justnobarongan@gmail.com>
2025-12-01 20:07:49 +0700

    Remove alias configuration from vite.config.js
    Removed path alias configuration for Vercel.

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Tambahkan alias untuk memastikan resolusi path di Vercel
-  resolve: {
-    alias: {
-      '/src': './src', 
-    }
-  }
+  // INI WAJIB UNTUK GITHUB PAGES. Ganti 'Landing_page' jika nama repo Anda berbeda.
+  base: '/Landing_page/', 
 })
